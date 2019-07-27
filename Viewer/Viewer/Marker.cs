c758c6d200d590f6de82f2e0daeea71a5908b34b
Vahid <vahid.ce@gmail.com>
2019-07-27 12:11:57 +0200

    add marker

---
+++ Viewer/Viewer/Marker.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
+
+namespace Viewer
+{
+    internal sealed class Marker : DrawingVisual
+    {
+        private const double Size = 5d;
+
+        private static readonly Vector s_p = new Vector(+0.25 * Math.PI, +0.25 * Math.PI) * Size;
+        private static readonly Vector s_q = new Vector(+0.25 * Math.PI, -0.25 * Math.PI) * Size;
+
+        public Marker(Pen pen, Point location)
+        {
+            using (DrawingContext drawingContext = RenderOpen())
+            {
+                drawingContext.DrawLine(pen, location + s_p, location - s_p);
+                drawingContext.DrawLine(pen, location + s_q, location - s_q);
+            }
+        }
+    }
+}
