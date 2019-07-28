23266f930e375da024f8c8005b7e73972ba31dce
Vahid <vahid.ce@gmail.com>
2019-07-28 14:59:54 +0200

    encapsuate commands

--- Viewer/Viewer/ApplicationModel.cs
+++ Viewer/Viewer/ApplicationModel.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Windows;
-using System.Windows.Input;
-using System.Windows.Media;
-using Newtonsoft.Json;
+﻿using System.Windows.Input;
 
 namespace Viewer
 {
@@ -20,74 +13,13 @@ namespace Viewer
 
         public ApplicationModel()
         {
-            LoadCommand = new Command(p => Load(), p => true);
-            ClearCommand = new Command(p => Clear(), p => true);
-            SaveJSONCommand = new Command(p => Save(), p => true);
+            RandomShapesCommand = new Command(p => { View = Commands.RandomShapes(); }, p => true);
+            ClearCommand = new Command(p => { View = Commands.Clear(); }, p => true);
+            LoadJsonCommand = new Command(p => { View = Commands.LoadJson(); }, p => true);
         }
 
-        public ICommand LoadCommand { get; }
+        public ICommand RandomShapesCommand { get; }
+        public ICommand LoadJsonCommand { get; }
         public ICommand ClearCommand { get; }
-        public ICommand SaveJSONCommand { get; }
-
-        private void Clear()
-        {
-            View = new View();
-        }
-
-        private void Save()
-        {
-            string path = "C:/backup/json.txt";
-
-            string jsonString = File.ReadAllText(path);
-
-            IList<ShapeDefinition> oo = JsonConvert.DeserializeObject<IList<ShapeDefinition>>(jsonString, new ShapeConverter());
-
-            IEnumerable<Shape> shapes = oo.Select(o => o.Convert());
-
-            var view = new View();
-            foreach (Shape shape in shapes)
-            {
-                view.AddShape(shape);
-            }
-
-            View = view;
-
-
-        }
-
-        private void Load()
-        {
-            var view = new View();
-
-            Random r = new Random();
-            int count = 0;
-            while (count < 100)
-            {
-                double x1 = r.NextDouble() * 1000;
-                double y1 = r.NextDouble() * 1000;
-                double x2 = r.NextDouble() * 1000;
-                double y2 = r.NextDouble() * 1000;
-
-                Pen pen = new Pen(Brushes.Red, r.NextDouble() * 10);
-                pen.Freeze();
-
-                var line = new Line(pen, new Point(x1, y1), new Point(x2, y2));
-                //view.DrawCircle(null, pen, new Point(r.NextDouble() * 1000, r.NextDouble() * 1000), r.NextDouble() * 100);
-
-
-                var a = new Point(r.NextDouble() * 1000, r.NextDouble() * 1000);
-                var b = new Point(r.NextDouble() * 1000, r.NextDouble() * 1000);
-                var c = new Point(r.NextDouble() * 1000, r.NextDouble() * 1000);
-
-                Polygon triangle = Polygon.Triangle(null, pen, a, b, c);
-
-                view.AddShape(line);
-                view.AddShape(triangle);
-
-                count++;
-            }
-
-            View = view;
-        }
     }
 }
---
+++ Viewer/Viewer/Commands.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using System.Windows.Media;
+using Newtonsoft.Json;
+
+namespace Viewer
+{
+    public static class Commands
+    {
+        public static View Clear()
+        {
+            return new View();
+        }
+
+        public static View LoadJson()
+        {
+            string path = "C:/backup/json.txt";
+
+            string jsonString = File.ReadAllText(path);
+
+            IList<ShapeDefinition> oo = JsonConvert.DeserializeObject<IList<ShapeDefinition>>(jsonString, new ShapeConverter());
+
+            IEnumerable<Shape> shapes = oo.Select(o => o.Convert());
+
+            var view = new View();
+            foreach (Shape shape in shapes)
+            {
+                view.AddShape(shape);
+            }
+
+            return view;
+        }
+
+        public static View RandomShapes()
+        {
+            var view = new View();
+
+            Random r = new Random();
+            int count = 0;
+            while (count < 100)
+            {
+                double x1 = r.NextDouble() * 1000;
+                double y1 = r.NextDouble() * 1000;
+                double x2 = r.NextDouble() * 1000;
+                double y2 = r.NextDouble() * 1000;
+
+                Pen pen = new Pen(Brushes.Red, r.NextDouble() * 10);
+                pen.Freeze();
+
+                var line = new Line(pen, new Point(x1, y1), new Point(x2, y2));
+                //view.DrawCircle(null, pen, new Point(r.NextDouble() * 1000, r.NextDouble() * 1000), r.NextDouble() * 100);
+
+
+                var a = new Point(r.NextDouble() * 1000, r.NextDouble() * 1000);
+                var b = new Point(r.NextDouble() * 1000, r.NextDouble() * 1000);
+                var c = new Point(r.NextDouble() * 1000, r.NextDouble() * 1000);
+
+                Polygon triangle = Polygon.Triangle(null, pen, a, b, c);
+
+                view.AddShape(line);
+                view.AddShape(triangle);
+
+                count++;
+            }
+
+            return view;
+        }
+    }
+}
